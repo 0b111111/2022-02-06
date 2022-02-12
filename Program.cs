48ba069c5e08b855ba96efedf90907cfa62fa193
Administrator <Administrator@TERMINATOR>
2022-02-12 02:21:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _2022_02_06
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.BackgroundColor=ConsoleColor.Black;
+            Int64 suma=0;
+            for(Int64 i=1;i<=100;i=suma-i)
+            {
+                Console.WriteLine(suma);
+                suma+=i;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
