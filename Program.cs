6dcceda7373b6d985ec78c36df8fb8484f620fba
Administrator <Administrator@TERMINATOR>
2022-02-12 02:21:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _2022_02_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.Green;
+            int suma=0;
+            for(int i=1;i<=100;i=suma-i)
+            {
+                Console.WriteLine("suma: "+suma+"\ni: "+i);
+                suma+=i;
+            }
+            //float myNum=1500000000;//,myNoNum=1500000000;
+            //Console.WriteLine(((decimal)myNum));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
